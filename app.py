9d66946b4cbed687939c33572bec81f11cc5f1b2
shawa84326 <148395690+shawa84326@users.noreply.github.com>
2024-02-17 01:21:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+import psycopg2
+
+# Load CSV data into DataFrame
+events_data = pd.read_csv('eventseattle.csv')
+
+# Establish a connection to the PostgreSQL database
+connection = psycopg2.connect(
+    user="xbmqnounao",
+    password="12D177O375IC8KO7$",
+    host="ankit25-lab5-redo-server.postgres.database.azure.com",
+    port=5432,
+    database="ankit25-lab5-redo-database"
+)
+# Insert data into the PostgreSQL database
+events_data.to_sql('events_table', connection, index=False, if_exists='replace')
+
+
 
 # Load data from PostgreSQL
 query = "SELECT * FROM events_table"
